f646deb304a7d3edc561ebb097bb91016940d69c
Jay Dodge <jaydodge1975@eastlink.ca>
2021-05-03 10:09:01 -0600

    Page Copied with loading issues

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// ! Change logo when navbar is at window top
+// window.addEventListener('scroll', () => {
+//   let windowH = window.innerHeight;
+//   const logo = document.getElementById("logo");
+//   const nav = document.getElementById("main-nav");
+//   nav.s
+//     if (window.scrollY > windowH) {
+//       // logo.src = "assets/images/home/logo2.png";
+//       logo.className = "logo logo-2";
+//     // } else if (nav) {
+//     //   logo.className = "logo-test";
+//     } else {
+//       // logo.src = "assets/images/home/logo1.png";
+//       logo.className = "logo logo-1";
+//     };
+// });
